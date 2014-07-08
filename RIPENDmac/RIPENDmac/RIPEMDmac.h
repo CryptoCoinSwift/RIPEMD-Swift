9915135b3c4f07b00585d4f0457d7c8f0be1cec6
Sjors Provoost <sjors@sprovoost.nl>
2014-07-08 13:15:55 +0200

    Xcode project with Cocoa framework

---
+++ RIPENDmac/RIPENDmac/RIPEMDmac.h
@@ -0,0 +1,19 @@
+//
+//  RIPENDmac.h
+//  RIPENDmac
+//
+//  Created by Sjors Provoost on 08-07-14.
+//  Copyright (c) 2014 Crypto Coin Swift. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RIPENDmac.
+FOUNDATION_EXPORT double RIPENDmacVersionNumber;
+
+//! Project version string for RIPENDmac.
+FOUNDATION_EXPORT const unsigned char RIPENDmacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RIPENDmac/PublicHeader.h>
+
+
