0ccb1b3904619bf7bf7cb424fc80271b840b1706
Sjors Provoost <sjors@sprovoost.nl>
2014-07-08 18:44:58 +0200

    Add block initializer. Move stuff around.

--- Classes/RIPEMD+Block.swift
+++ Classes/RIPEMD+Block.swift
@@ -10,57 +10,12 @@ import Foundation
 
 extension RIPEMD {
     struct Block {
-        struct f {
-            
-            // Unfortunately static subscript is not allowed
-            subscript(j: Int) -> ((UInt32, UInt32, UInt32) -> UInt32) {
-                switch j {
-                case let index where j < 0:
-                    assert("Invalid j")
-                    return {(_, _, _) in 0 }
-                case let index where j <= 15:
-                    return {(x, y, z) in  x ^ y ^ z }
-                case let index where j <= 31:
-                    return {(x, y, z) in  (x & y) | (~x & z) }
-                case let index where j <= 47:
-                    return {(x, y, z) in  (x | ~y) ^ z }
-                case let index where j <= 63:
-                    return {(x, y, z) in  (x & z) | (y & ~z) }
-                case let index where j <= 79:
-                    return {(x, y, z) in  x | (y | ~z) }
-                default:
-                    assert("Invalid j")
-                    return {(_, _, _) in 0 }
-                    }
-                    
-                    
-            }
-        }
-
+        let message: [UInt32]
         
-        enum K {
-            case Left, Right
-        
-            subscript(j: Int) -> UInt32 {
-                switch index {
-                case let index where j < 0:
-                    assert("Invalid j")
-                    return 0
-                case let index where j <= 15:
-                    return self == .Left ? 0x00000000 : 0x50A28BE6
-                case let index where j <= 31:
-                    return self == .Left ? 0x5A827999 : 0x5C4DD124
-                case let index where j <= 47:
-                    return self == .Left ? 0x6ED9EBA1 : 0x6D703EF3
-                case let index where j <= 63:
-                    return self == .Left ? 0x8F1BBCDC : 0x7A6D76E9
-                case let index where j <= 79:
-                    return self == .Left ? 0xA953FD4E : 0x00000000
-                default:
-                    assert("Invalid j")
-                    return 0
-                }
-            }
+        init(_ message: [UInt32]) {
+            assert(countElements(message) == 16, "Wrong message size")
+            self.message = message
         }
+        
     }
 }
--- Tests/RIPEMD-Block-Tests.swift
+++ Tests/RIPEMD-Block-Tests.swift
@@ -24,17 +24,20 @@ class RIPEMD_Block_Tests: XCTestCase {
 
     func testAddedConstants() {
         let k40:UInt32 = 0x6ED9EBA1
-        XCTAssertEqual(RIPEMD.Block.K.Left[40], k40, "");
+        XCTAssertEqual(RIPEMD.K.Left[40], k40, "");
     }
     
-    func testBitlevelFunctions() {
+    func testBitlevelFunctions() {        
         let x: UInt32 =       0b0000_0000_0000_1111
         let y: UInt32 =       0b1111_1111_1111_0000
         let z: UInt32 =       0b0000_1111_1111_0000
         
         let xORxOR: UInt32 =  0b1111_0000_0000_1111
         
-        XCTAssertEqual(RIPEMD.Block.f()[12](x,y,z), xORxOR, "")
+        let function = RIPEMD.f(12)
+        let result = function(x,y,z)
+        
+        XCTAssertEqual(result, xORxOR, "")
     }
     
 }
