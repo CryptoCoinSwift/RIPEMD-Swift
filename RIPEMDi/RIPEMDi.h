0b25b45042a4b600fb3017c521cf44e642630092
Sjors Provoost <sjors@sprovoost.nl>
2014-08-03 17:57:06 +0200

    Missing files

---
+++ RIPEMDi/RIPEMDi.h
@@ -0,0 +1,12 @@
+//
+//  RIPEMD.h
+//  RIPEMD
+//
+//  Created by Sjors Provoost on 03-08-14.
+//  Copyright (c) 2014 Crypto Coin Swift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+FOUNDATION_EXPORT double RIPEMDiVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char RIPEMDiVersionString[];
